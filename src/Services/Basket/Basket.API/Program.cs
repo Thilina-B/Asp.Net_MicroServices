ce88fd258c88501a8658d939a2bd8170ddba9107
Thilina Rathnayake <thilinaswift@gmail.com>
2022-08-10 00:27:31 +0530

    Test rebatimy on basket api

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ builder.Services.AddControllers();
 ConfigurationManager Configuration = builder.Configuration;
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//grcp configuration
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
 builder.Services.AddScoped<DiscountGrpcService>();
 builder.Services.AddStackExchangeRedisCache(options =>
@@ -19,6 +22,15 @@ builder.Services.AddStackExchangeRedisCache(options =>
 
 });
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddAutoMapper(typeof(Program));
+
+// MassTransit-RabbitMQ Configuration
+builder.Services.AddMassTransit(config => {
+    config.UsingRabbitMq((ctx, cfg) => {
+        cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+    });
+});
+
 
 var app = builder.Build();
 
