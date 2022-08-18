27780fc6eeabd25f17d8c6595be070515c5f2e11
Thilina Rathnayake <thilinaswift@gmail.com>
2022-08-19 00:34:56 +0530

    Shopping Aggregator

--- src/ApiGateways/Shopping.Aggregator/Program.cs
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -1,6 +1,17 @@
+using Shopping.Aggregator.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager Configuration = builder.Configuration;
 
 // Add services to the container.
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(c =>
+               c.BaseAddress = new Uri(Configuration["ApiSettings:CatalogUrl"]));
+
+builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
+    c.BaseAddress = new Uri(Configuration["ApiSettings:BasketUrl"]));
+
+builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
+    c.BaseAddress = new Uri(Configuration["ApiSettings:OrderingUrl"]));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
